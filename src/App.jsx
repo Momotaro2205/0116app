7a91793c20b31f2e1e602ca3aefe5c19d196a971
Momotaro2205 <168008425+Momotaro2205@users.noreply.github.com>
2024-06-19 09:37:55 +0300

    4 components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { NavLink, Route, Routes } from "react-router-dom";
-//компонент profile
-
-const Profile = () => {
-  return <h2>Страница профиля</h2>;
-};
+import { Profile } from "./components/Profile";
 
 function App() {
   return (
